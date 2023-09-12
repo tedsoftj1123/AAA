8da4708700f82f29889293b368939b40b9a53400
tedsoftj1123 <tedsoftj1123@gmail.com>
2023-09-12 19:20:46 +0900

    feat: parse token

--- src/application/auth/spi/auth.spi.ts
+++ src/application/auth/spi/auth.spi.ts
@@ -2,6 +2,7 @@ import { TokenResponse } from '../dto/auth.dto';
 
 export interface JwtPort {
   generateToken(userId: string): Promise<TokenResponse>;
+  parseJwt(token: string): Promise<string>;
 }
 
 export const JwtPort = Symbol('IJwtPort');
