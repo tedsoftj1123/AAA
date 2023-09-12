54834b52803701121f19fcbf17c8142365575d67
tedsoftj1123 <tedsoftj1123@gmail.com>
2023-09-12 19:45:25 +0900

    feat: validation

--- src/presentation/user/user.dto.ts
+++ src/presentation/user/user.dto.ts
@@ -1,5 +1,22 @@
+import { IsEmail, IsNotEmpty } from 'class-validator';
+
 export class SignupRequest {
+  @IsNotEmpty()
+  @IsEmail()
   email: string;
+
+  @IsNotEmpty()
   password: string;
+
+  @IsNotEmpty()
   name: string;
+}
+
+export class LoginRequest {
+  @IsEmail()
+  @IsNotEmpty()
+  email: string;
+
+  @IsNotEmpty()
+  password: string;
 }
